9fde27e69fb6f81be296d02d615e30b5848ccecb
Bernie <bsauce@gmail.com>
2018-02-08 14:06:21 -0500

    Fixed setup.py for Linux.

--- setup.py
+++ setup.py
@@ -11,7 +11,13 @@ from setuptools.extension import Extension
 from Cython.Build import cythonize
 
 import numpy as np
+import os
 
+# set language argument based on OS
+if os.name == 'nt':
+    _LANGUAGE = 'c++' # for MSVC++
+else:
+    _LANGUAGE = 'c' # for gcc
 
 ext_modules = [
     Extension(name = 'interaction3.bem.core.fma_functions',
