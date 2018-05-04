738e18f0823699539d54aa10ea4e5700b6a793f8
Bernie <bsauce@gmail.com>
2018-05-04 15:51:04 -0400

    Added more efficient resampling for time beamformer

--- interaction3/reconstruction/beamforming.py
+++ interaction3/reconstruction/beamforming.py
@@ -1,109 +1,118 @@
 # reconstruction / beamformer.py
 
-__all__ = ['beamform']
+# __all__ = ['beamform']
 
 import numpy as np
 import scipy as sp
 from scipy.spatial.distance import cdist as distance
 import attr
 
-try:
-    import engines
-    _ENGINES_PYX_PRESENT = True
-except ImportError:
-    _ENGINES_PYX_PRESENT = False
+# try:
+#     import engines
+#     _ENGINES_PYX_PRESENT = True
+# except ImportError:
+#     _ENGINES_PYX_PRESENT = False
+#
+#
+# def beamform(rfdata, fieldpos, **kwargs):
+#
+#     # get data attribtues and beamforming options
+#     txpos = kwargs.get('txpos', None)
+#     rxpos = kwargs.get('rxpos')
+#     nwin = int(kwargs.get('nwin', 51))
+#     fs = kwargs.get('fs')
+#     c = kwargs.get('c')
+#     resample = kwargs.get('resample', False)
+#
+#     chmask = kwargs.get('chmask', False)
+#     t0 = kwargs.get('t0', 0.0)
+#     apod = kwargs.get('apod', False)
+#     ram_limit = kwargs.get('ram_limit', 2.0)
+#
+#     # elevate arrays to 2d if needed
+#     fieldpos, rxpos = np.atleast_2d(fieldpos, rxpos)
+#
+#     if rfdata.ndim == 3:
+#         nsample, nchannel, nframe = rfdata.shape
+#     elif rfdata.ndim == 2:
+#         nsample, nchannel = rfdata.shape
+#         rfdata = rfdata[..., None]
+#
+#     # set defaults
+#     if chmask is False:
+#         chmask = np.ones(nchannel)
+#     if apod is False:
+#         apod = np.ones(nchannel)
+#     if resample is False:
+#         resample = 1
+#
+#     # resample data
+#     if resample != 1:
+#         rfdata = sp.signal.resample(rfdata, nsample * resample)
+#         nsample = rfdata.shape[0]
+#
+#     if not nwin % 2:
+#         nwin += 1
+#
+#     # pad data
+#     pad_width = ((nwin, nwin), (0, 0), (0, 0))
+#     rfdata = np.pad(rfdata, pad_width, mode='constant')
+#
+#     # select beamforming engine
+#     if _ENGINES_PYX_PRESENT:
+#
+#         engine = engines._time_beamform_engine
+#         chmask = chmask.astype(np.int32)
+#         apod = apod.astype(np.float64)
+#
+#     else:
+#         engine = _time_beamform_engine
+#
+#     # calculate transmit delays
+#     if txpos is None:
+#         txdelay = np.zeros((fieldpos.shape[0], 1), dtype=np.float64)
+#     elif txpos in ['plane', 'planetx', 'planewave', 'pw']:
+#         txdelay = np.abs(fieldpos[:, 2:3]) / c
+#     else:
+#         txpos = np.atleast_2d(txpos)
+#         txdelay = distance(fieldpos, txpos) / c
+#
+#     # split fieldpos array to reduce memory usage
+#     npos = fieldpos.shape[0]
+#     nrx = rxpos.shape[0]
+#     nsplit = int(np.ceil((npos * nrx * 8 / 1000 / 1000 / 1000) / ram_limit))
+#
+#     result = list()
+#     for fp, td in zip(np.array_split(fieldpos, nsplit, axis=0), np.array_split(txdelay, nsplit, axis=0)):
+#
+#         rd = distance(fp, rxpos) / c
+#         delays = np.round((td + rd - t0) * fs * resample).astype(np.int32)
+#
+#         bfdata = engine(rfdata, delays, nwin, chmask, apod)
+#         result.append(bfdata)
+#
+#     return np.concatenate(result, axis=0)
 
+def _rfdata_converter(value):
+    if value.ndim == 2:
+        return value[..., None]
 
-def beamform(rfdata, fieldpos, **kwargs):
+def _window_converter(value):
+    if not value % 2:
+        value += 1
 
-    # get data attribtues and beamforming options
-    txpos = kwargs.get('txpos', None)
-    rxpos = kwargs.get('rxpos')
-    nwin = int(kwargs.get('nwin', 51))
-    fs = kwargs.get('fs')
-    c = kwargs.get('c')
-    resample = kwargs.get('resample', False)
-
-    chmask = kwargs.get('chmask', False)
-    t0 = kwargs.get('t0', 0.0)
-    apod = kwargs.get('apod', False)
-    ram_limit = kwargs.get('ram_limit', 2.0)
-
-    # elevate arrays to 2d if needed
-    fieldpos, rxpos = np.atleast_2d(fieldpos, rxpos)
-
-    if rfdata.ndim == 3:
-        nsample, nchannel, nframe = rfdata.shape
-    elif rfdata.ndim == 2:
-        nsample, nchannel = rfdata.shape
-        rfdata = rfdata[..., None]
-
-    # set defaults
-    if chmask is False:
-        chmask = np.ones(nchannel)
-    if apod is False:
-        apod = np.ones(nchannel)
-    if resample is False:
-        resample = 1
-
-    # resample data
-    if resample != 1:
-        rfdata = sp.signal.resample(rfdata, nsample * resample)
-        nsample = rfdata.shape[0]
-
-    if not nwin % 2:
-        nwin += 1
-
-    # pad data
-    pad_width = ((nwin, nwin), (0, 0), (0, 0))
-    rfdata = np.pad(rfdata, pad_width, mode='constant')
-
-    # select beamforming engine
-    if _ENGINES_PYX_PRESENT:
-        
-        engine = engines._time_beamform_engine
-        chmask = chmask.astype(np.int32)
-        apod = apod.astype(np.float64)
-        
-    else:
-        engine = _time_beamform_engine
-
-    # calculate transmit delays
-    if txpos is None:
-        txdelay = np.zeros((fieldpos.shape[0], 1), dtype=np.float64)
-    elif txpos in ['plane', 'planetx', 'planewave', 'pw']:
-        txdelay = np.abs(fieldpos[:, 2:3]) / c
-    else:
-        txpos = np.atleast_2d(txpos)
-        txdelay = distance(fieldpos, txpos) / c
-
-    # split fieldpos array to reduce memory usage
-    npos = fieldpos.shape[0]
-    nrx = rxpos.shape[0]
-    nsplit = int(np.ceil((npos * nrx * 8 / 1000 / 1000 / 1000) / ram_limit))
-
-    result = list()
-    for fp, td in zip(np.array_split(fieldpos, nsplit, axis=0), np.array_split(txdelay, nsplit, axis=0)):
-
-        rd = distance(fp, rxpos) / c
-        delays = np.round((td + rd - t0) * fs * resample).astype(np.int32)
-
-        bfdata = engine(rfdata, delays, nwin, chmask, apod)
-        result.append(bfdata)
-
-    return np.concatenate(result, axis=0)
-
+    return int(value)
 
 @attr.s
-class Beamformer:
+class Beamformer(object):
 
-    rfdata = attr.ib(init=True)
+    rfdata = attr.ib(init=True, converter=_rfdata_converter)
     field_pos = attr.ib(init=True, converter=np.atleast_2d)
     receive_pos = attr.ib(init=True, converter=np.atleast_2d)
     transmit_pos = attr.ib(init=True, converter=np.atleast_2d, default=None)
     planewave = attr.ib(init=True, default=False)
     angles = attr.ib(init=True, default=None)
-    window = attr.ib(init=True, default=51)
+    window = attr.ib(init=True, converter=_window_converter, default=51)
     sample_frequency = attr.ib(init=True, default=40e6)
     sound_speed = attr.ib(init=True, default=1540)
     apodization = attr.ib(init=True)
@@ -114,19 +123,10 @@ class Beamformer:
 
     result = attr.ib(init=False, default=attr.Factory(dict))
 
-    @rfdata.converter
-    def _rfdata_converter(self, attribute, value):
-        if value.ndim == 2:
-            return value[..., None]
-
     @rfdata.validator
     def _rfdata_validator(selfself, attribute, value):
         if value.ndim != 3:
             raise ValueError('rfdata must have dimensions 3')
-
-    @transmit_pos.default
-    def _transmit_pos_default(self):
-        pass
 
     @channel_mask.default
     def _channel_mask_default(self):
@@ -138,10 +138,12 @@ class Beamformer:
         nchannels = self.rfdata.shape[1]
         return np.ones(nchannels)
 
-    @window.converter
-    def _window_converter(self, attribute, value):
-        if not value % 2:
-            value += 1
+    @transmit_pos.validator
+    def _transmit_pos_validator(self, attribute, value):
+
+        if self.planewave:
+            if value.shape != (1, 3):
+                raise ValueError('For planewave, transmit_pos must be a single position')
 
     def run(self):
 
@@ -187,7 +189,7 @@ class Beamformer:
                 angles = [0,]
 
             rad_angles = np.deg2rad(angles)
-            normals = np.c_[np.cos(rad_angles), np.sin(rad_angles), 0]
+            normals = np.c_[np.cos(rad_angles), np.zeros(len(rad_angles)), np.sin(rad_angles)]
             r = field_pos - transmit_pos
             transmit_delays = np.abs(np.dot(r, normals.T)) / sound_speed
             transmit_delays = transmit_delays[:, None, :]
@@ -201,10 +203,14 @@ class Beamformer:
                 transmit_delays = distance(field_pos, transmit_pos) / sound_speed
                 transmit_delays = transmit_delays[:, None, :]
 
+        # calculate receive delays
         receive_delays = distance(field_pos, receive_pos) / sound_speed
         receive_delays = receive_delays[..., None]
 
+        # calculate total delays
         delays = transmit_delays + receive_delays - t0
+
+        return delays
 
         # split fieldpos array to reduce memory usage
         # npos = field_pos.shape[0]
@@ -224,27 +230,18 @@ class Beamformer:
 
 
 
-
-
-
 if __name__ == '__main__':
 
-    pass
+    prms = dict()
+    # prms['transmit_pos'] = np.c_[np.linspace(-0.02, 0.02, 16), np.zeros(16), np.zeros(16)]
+    prms['transmit_pos'] = [0, 0, 0]
+    prms['receive_pos'] = np.c_[np.linspace(-0.02, 0.02, 16), np.zeros(16), np.zeros(16)]
+    x, y, z = np.mgrid[-0.02:0.02:11j, 0:1:1j, 0.02:0.04:11j]
+    prms['field_pos'] = np.c_[x.ravel(), y.ravel(), z.ravel()]
+    prms['rfdata'] = sp.rand(1000, 16)
+    prms['planewave'] = True
+    prms['angles'] = np.linspace(-20, 20, 3)
 
-    # prms = dict()
-    # prms['txpos'] = np.c_[np.linspace(-0.02, 0.02, 676), np.zeros(676), np.zeros(676)]
-    # prms['rxpos'] = np.c_[np.linspace(-0.02, 0.02, 676), np.zeros(676), np.zeros(676)]
-    # x, y, z = np.mgrid[-0.02:0.02:60j, -0.02:0.02:60j, 0.001:0.041:60j]
-    # prms['fieldpos'] = np.c_[x.ravel(), y.ravel(), z.ravel()]
-    # prms['rfdata'] = sp.rand(3000, 676)
-    # prms['fs'] = 100e6
-    # prms['c'] = 1540.
-    
-    #delays = np.round(sp.rand(1, 676)*100).astype(np.int32)
-    #nwin = int(51)
-    #chmask = np.ones(676, dtype=np.int32)
-    #apod = np.ones(676, dtype=np.float64)
-    
-    #bfdata = beamform(**prms)
-
+    bf = Beamformer(**prms)
+    delays = bf.run()
     
--- interaction3/reconstruction/engines.py
+++ interaction3/reconstruction/engines.py
@@ -1,46 +1,78 @@
 
 import numpy as np
+from scipy import signal
 
-try:
-    import engines
-    _ENGINES_PYX_PRESENT = True
-except ImportError:
-    _ENGINES_PYX_PRESENT = False
+# try:
+#     import engines
+#     _ENGINES_PYX_PRESENT = True
+# except ImportError:
+#     _ENGINES_PYX_PRESENT = False
 
 
-def _time_beamform(rfdata, delays, nwin, channel_mask, apodization):
+def _time_beamform(rfdata, delays, window, channel_mask, apodization, sample_frequency, resample):
 
     # get data attribtues and beamforming options
-    npos = delays.shape[0]
+    npos, _, ndelay_frames = delays.shape
     nsamples, nchannels, nframes = rfdata.shape
-    nsamples = nsamples - 2 * nwin
-    nwinhalf = (nwin - 1) // 2
+    nsamples = nsamples - 2 * window
+    winhalf = (window - 1) // 2
 
-    bfdata = np.zeros((npos, nwin, nframes))
+    if resample > 1:
 
-    # apply delays in loop over field points and channels
-    for pos in range(npos):
+        # use floor if resampling to keep windowing logic simple
+        sample_delays = np.floor(delays * sample_frequency).astype(int)
+        resample_delays = np.round(delays * sample_frequency * resample).astype(int)
+        resample_window = winhalf * resample * 2 + 1
+        bfdata = np.zeros((npos, window * resample, nframes))
+    else:
+        sample_delays = np.round(delays * sample_frequency).astype(int)
+        bfdata = np.zeros((npos, window, nframes))
 
-        sd = delays[pos, :]
-        valid_delay = (sd <= (nsamples + nwinhalf)) & (sd >= -(nwinhalf + 1))
+    if ndelay_frames == 1:
 
-        if not np.any(valid_delay):
-            continue
+        for pos in range(npos):
 
-        bfsig = np.zeros((nwin, nframes))
+            sd = sample_delays[pos, :, 0]
+            valid_delay = (sd <= (nsamples + winhalf)) & (sd >= -(winhalf + 1))
 
-        for ch in range(nchannels):
-
-            if not valid_delay[ch]:
+            if not np.any(valid_delay):
                 continue
 
-            if not channel_mask[ch]:
-                continue
+            if resample > 1:
+                rsd = resample_delays[pos, :, 0]
+                bfsig = np.zeros((resample_window, nframes))
+            else:
+                bfsig = np.zeros((window, nframes))
 
-            delay = sd[ch] + nwin - nwinhalf
+            for ch in range(nchannels):
 
-            bfsig += apodization[ch] * rfdata[delay:(delay + nwin), ch, :]
+                if not valid_delay[ch]:
+                    continue
 
-        bfdata[pos, :, :] = bfsig
+                if not channel_mask[ch]:
+                    continue
+
+                delay = sd[ch] + window - winhalf # account for zero-padding
+                window_rf = rfdata[delay:(delay + window), ch, :]
+
+                if resample > 1:
+
+                    resample_rf = signal.resample(window_rf, window * resample, axis=0)
+                    start = rsd[ch] - (resample * sd[ch])
+                    rf = resample_rf[start:(start + resample_window), :]
+                else:
+
+                    rf = window_rf
+
+                bfsig += apodization[ch] * rf
+
+            bfdata[pos, :, :] = bfsig
+
+    else:
+
+        for frame in range(nframes):
+            for pos in range(npos):
+                for ch in range(nchannels):
+                    pass
 
     return bfdata
